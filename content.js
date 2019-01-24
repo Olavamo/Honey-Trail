4b61a5bab5a9bedb9bbf19b3b226d299b03326f5
Olavamo <32780510+Olavamo@users.noreply.github.com>
2019-01-23 20:33:59 -0600

    Add files via upload

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-alert("You're on the free-trial page!")
+// window.open('https://google.com', "_blank")
+
+var myWindow = window.open("", "MsgWindow", "width=400,height=200");
+myWindow.document.write("<p><b>It's on the House!</b></p><p>We'll set a reminder for your Netflix subscription.</p>\n<p>We will remind you before you are charged.</p>");
+
+// alert("You're on the free-trial page!")
+
